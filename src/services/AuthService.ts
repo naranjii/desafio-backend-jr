4692dd2e0dfb03e5f46527c33aedfc187bf5c594
laranjeira <maat.laranjeira@gmail.com>
2025-09-12 04:31:08 +0000

    fix: UserRole type to AuthService + fixes missing argument at unitary test

--- src/services/AuthService.ts
+++ src/services/AuthService.ts
@@ -2,10 +2,11 @@ import { prisma } from "../config/db";
 import * as bcrypt from 'bcryptjs';
 import jwt from "jsonwebtoken";
 import { UserRepository } from "../repositories/UserRepository";
+import { UserRole } from "../generated/prisma";
 
-export async function register(name: string, email: string, password: string, role: string = "consultant") {
+export async function register(name: string, email: string, password: string, role: UserRole) {
   const hashed = await bcrypt.hash(password, 10);
-  return UserRepository.create({ name, email, role: role as "consultant", hashedPassword: hashed })
+  return UserRepository.create({ name, email, role, hashedPassword: hashed })
 }
 
 export async function login(email: string, password: string) {
