4e6286a1fd86b57bf1d3823909e428a7bdffd7d7
laranjeira <maat.laranjeira@gmail.com>
2025-09-12 04:55:24 +0000

    feat: +RequestRepository.ts | +/request route => app.ts | +approvalStatus enum => schema.prisma

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,9 @@
 import express from "express";
 import authRoutes from "./routes/authRoutes";
+import requestRoutes from "./routes/requestRoutes";
 
 const app = express();
 app.use(express.json());
 app.use("/auth", authRoutes);
-
+app.use("/requests", requestRoutes)
 export default app;
