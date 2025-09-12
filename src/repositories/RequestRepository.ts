82ae6d40992dc97e16b228ea58864257868909d2
laranjeira <maat.laranjeira@gmail.com>
2025-09-12 14:32:33 +0000

    docs: route commentaries

--- src/repositories/RequestRepository.ts
+++ src/repositories/RequestRepository.ts
@@ -3,27 +3,27 @@ import { ApprovalStatus } from "../generated/prisma";
 import { RequestItem } from "../models/RequestItem";
 
 export const RequestRepository = {
-    async create({ userId, requestItem }: { userId: string, requestItem: RequestItem }) {
+    async create({ userId, requestItem }: { userId: string, requestItem: RequestItem }) {   // => POST /requests
         return prisma.purchaseRequest.create({
             data: { userId, status: ApprovalStatus.draft, items: { create: requestItem } }
         });
     },
-    async update({ id, requestItem }: { id: string, requestItem: RequestItem }) {
+    async update({ id, requestItem }: { id: string, requestItem: RequestItem }) {           // => PATCH /requests/:id
         return prisma.purchaseRequest.update({
             where: { id },
             data: { items: { create: requestItem } }
         });
     },
 
-    async getAll() {
+    async getAll() {                                                                        // => GET /requests
         return prisma.purchaseRequest.findMany();
     },
 
-    async getById(id: string) {
+    async getById(id: string) {                                                             // => GET /requests/:id
         return prisma.purchaseRequest.findUnique({ where: { id } })
     },
 
-    async getByStatus() {
+    async getByStatus() {                                                                   // => GET /reports/summary  
         return prisma.purchaseRequest.groupBy({
             by: ["status"],
             _count: { status: true }
