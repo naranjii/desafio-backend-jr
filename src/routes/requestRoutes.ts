ee2239a6e4e5a08c74363a433855e7c195910109
laranjeira <maat.laranjeira@gmail.com>
2025-09-12 19:37:25 +0000

    fix: declined => rejected + slight additions to /requests/ path

--- src/routes/requestRoutes.ts
+++ src/routes/requestRoutes.ts
@@ -3,9 +3,11 @@ import * as RequestController from "../controllers/RequestController";
 import { authMiddleware } from "../middlewares/authMiddleware";
 const router = Router();
 router.use(authMiddleware);
-router.post("/", RequestController.create);
 router.get("/", RequestController.list);
 router.get("/:id", RequestController.getById);
-router.patch("/:id", RequestController.update);
+router.post("/", RequestController.create);
 router.post("/:id/submit", RequestController.submit)
+router.post("/:id/approve", RequestController.approve);
+router.post("/:id/reject", RequestController.reject);
+router.patch("/:id", RequestController.update);
 export default router;
