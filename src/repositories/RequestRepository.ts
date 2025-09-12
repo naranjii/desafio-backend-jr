9ae4b0b43b97bd22ea1d2d2849b300ad4e90f058
laranjeira <maat.laranjeira@gmail.com>
2025-09-12 04:55:24 +0000

    feat: +RequestRepository.ts | +/request route => app.ts | +approvalStatus enum => schema.prisma

---
+++ src/repositories/RequestRepository.ts
@@ -0,0 +1,16 @@
+import { prisma } from "../config/db";
+import { approvalStatus, PrismaClient, UserRole } from "../generated/prisma";
+
+export const RequestRepository = {
+    async create({ id, status, userId }: { id: string, status: approvalStatus, userId: string }) {
+        return prisma.purchaseRequest.create({
+            data: { id, userId, status: 'draft' }
+        });
+    },
+    async getAll() {
+        return prisma.purchaseRequest.findMany();
+    },
+    async getById(id: string) {
+        return prisma.purchaseRequest.findUnique({ where: { id } })
+    }
+}
