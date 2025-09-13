f3d77cc9ae97f5c47980a2f797df2fbc88095c19
laranjeira <maat.laranjeira@gmail.com>
2025-09-13 07:57:09 +0000

    feat: swagger

--- src/app.ts
+++ src/app.ts
@@ -2,10 +2,15 @@ import express from "express";
 import authRoutes from "./routes/authRoutes";
 import requestRoutes from "./routes/requestRoutes";
 import reportRoutes from "./routes/reportRoutes"
+import swaggerUi from 'swagger-ui-express';
+import { openApiDoc } from "./openapi/_config";
+import { createDocument } from "zod-openapi";
+import z from "zod";
 
 const app = express();
 app.use(express.json());
 app.use("/auth", authRoutes);
 app.use("/requests", requestRoutes)
 app.use("/reports", reportRoutes)
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(openApiDoc));
 export default app;
