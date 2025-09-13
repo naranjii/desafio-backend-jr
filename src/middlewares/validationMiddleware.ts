0b3dacda505e24bb896a9fb3bc2d0036a54a0940
laranjeira <maat.laranjeira@gmail.com>
2025-09-13 04:11:29 +0000

    feat: create TypedRequest interface and validationMiddleware

--- src/middlewares/validationMiddleware.ts
+++ src/middlewares/validationMiddleware.ts
@@ -1,7 +1,7 @@
 import { NextFunction, Request, Response } from "express";
 import { z, ZodError } from "zod"
 
-export function validateData(schema: z.ZodObject<any, any>) {
+export function validationMiddleware(schema: z.ZodObject<any, any>) {
     return (req: Request, res: Response, next: NextFunction) => {
         try {
             schema.parse(req.body);
